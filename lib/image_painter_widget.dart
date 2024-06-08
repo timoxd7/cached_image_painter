baff7c9cff9c37ca5f610d2cfe0b3d159c962e23
timoxd7 <timoxd7@gmx.de>
2024-06-08 16:31:59 +0200

    Final

--- lib/image_painter_widget.dart
+++ lib/image_painter_widget.dart
@@ -77,24 +77,24 @@ class _ImagePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint();
-    double imageRatio = image.width / image.height;
-    double canvasRatio = size.width / size.height;
+    final Paint paint = Paint();
+    final double imageRatio = image.width / image.height;
+    final double canvasRatio = size.width / size.height;
 
     Rect src;
     if (imageRatio > canvasRatio) {
       // Image is wider than canvas, so cut off the left and right sides
-      double excessWidth = image.height * canvasRatio;
-      double offsetX = (image.width - excessWidth) / 2;
+      final double excessWidth = image.height * canvasRatio;
+      final double offsetX = (image.width - excessWidth) / 2;
       src = Rect.fromLTWH(offsetX, 0, excessWidth, image.height.toDouble());
     } else {
       // Image is taller than canvas, so cut off the top and bottom
-      double excessHeight = image.width / canvasRatio;
-      double offsetY = (image.height - excessHeight) / 2;
+      final double excessHeight = image.width / canvasRatio;
+      final double offsetY = (image.height - excessHeight) / 2;
       src = Rect.fromLTWH(0, offsetY, image.width.toDouble(), excessHeight);
     }
 
-    Rect dst = Rect.fromLTWH(0, 0, size.width, size.height);
+    final Rect dst = Rect.fromLTWH(0, 0, size.width, size.height);
     canvas.drawImageRect(image, src, dst, paint);
   }
 
